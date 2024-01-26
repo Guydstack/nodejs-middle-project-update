d5435dddc6b8de57adf8ee1bc8be62386147dd3d
Guydstack <129586924+Guydstack@users.noreply.github.com>
2024-01-26 18:35:28 +0200

    initial commit

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const events_router = require("./routes/events");
 const orders_router = require("./routes/orders");
 
 app.use(cors({
-  origin: /https:\/\/nodejs-middle-project-client-update\.onrender\.com$/, // Update to match your client's domain
+  origin: "*",
   credentials: true,
 }));
 
